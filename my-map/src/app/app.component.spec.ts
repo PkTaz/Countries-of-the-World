18334322b6088b6a3ac674e1929c6710ccd75d6c
wchap23 <wchap23@wgu.edu>
2024-09-29 22:06:29 -0400

    updated component.html, spec.ts, and comp.ts

--- my-map/src/app/app.component.spec.ts
+++ my-map/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
       imports: [AppComponent],
     }).compileComponents();
   });
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-map' title`, () => {
+  it(`should have the 'd280_app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-map');
+    expect(app.title).toEqual('d280_app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-map');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, d280_app');
   });
 });
--- my-map/src/app/app.component.ts
+++ my-map/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WorldComponent } from "./world/world.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WorldComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-map';
+  title = 'd280_app';
 }
